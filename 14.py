83d0ad00fd21b27675abc6be11e69390a82fc873
avaziyi <lzy_8921@163.com>
2013-11-03 21:35:32 +0800

    README.md

--- 14.py
+++ 14.py
@@ -6,6 +6,13 @@ Created on Sat Nov 02 16:10:03 2013
 """
 
 import this
-#print this
+s=this.s
+d = {}
+for c in (65, 97):
+    for i in range(26):
+        d[chr(i+c)] = chr((i+13) % 26 + c)
+
+#print u'python 之禅' 
+print "".join([d.get(c, c) for c in s])
 
 #print u'Python之禅'
---
+++ 34.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 03 20:42:02 2013
+
+@author: 子怿
+"""
+
+#import base64
+#print "ACCESS_KEY"
+#exec base64.b64decode('aW1wb3J0IGJhZTtwcmludCBiYWUuY29yZS5jb25zdC5BQ0NFU1NfS0VZ')
+#print "APPID"
+#exec base64.b64decode('aW1wb3J0IGJhZTtwcmludCBiYWUuY29yZS5jb25zdC5BUFBJRA==')
